4f14b2850ab924d2849a0ef24ea787f832f0d8b6
Camila Caprino <camicaprino@gmail.com>
2026-01-13 12:45:52 -0300

    Fix PostCSS config and enable Tailwind styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Upgrade from "@/components/Home/upgrade";
 import Perks from "@/components/Home/perks";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "CrypGo",
+  title: "landing NR",
 };
 
 export default function Home() {
